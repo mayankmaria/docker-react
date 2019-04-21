2386e83b1cd759662b03dc082fac89deab8a5d82
mayankmaria <mayank_maria@hotmail.com>
2019-04-21 21:37:42 +0530

    pushed to feature

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. hellHi
+            Edit <code>src/App.js</code> and save to reload. This is deployed in AWS.
           </p>
           <a
             className="App-link"
